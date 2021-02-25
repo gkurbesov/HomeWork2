83216fa752b419bf6be465e6fe43229fbeeea0de
Gennady <ltrg67@gmail.com>
2021-02-25 11:13:54 +0300

    Добавьте файлы проекта.

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Код к первому заданию
+             */
+        }
+    }
+}
---
+++ HomeWork2_2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HomeWork2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Код ко второму заданию
+             */
+        }
+    }
+}
